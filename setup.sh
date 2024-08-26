a47683ace071bfde127afb29e04be525c91508aa
Toodie <lucas.vtodt@gmail.com>
2024-08-26 10:11:39 -0300

    add configuration of an env var named SU_BIN_FOLDER
    Details:
    - Define path of utils folder and store in SU_BIN_FOLDER
    - Updates .bashrc with the SU_BIN_FOLDER using IO rediction(>>)
    
    Benefits:
    - Makes scripts inside /bin in the project accessible in the terminal regardless its location

--- setup.sh
+++ setup.sh
@@ -16,4 +16,16 @@ else
     echo -e "  > Already configured SU_UTILS_FOLDER!\n"
 fi
 
+if [[ -z "$SU_BIN_FOLDER" ]]; then
+
+    # shellcheck disable=SC2155
+    readonly SU_BIN_FOLDER=$(pwd)"/bin"
+
+    echo "export SU_BIN_FOLDER=""$SU_BIN_FOLDER" >> ~/.bashrc
+    echo "export PATH=""$PATH":"$SU_BIN_FOLDER" >> ~/.bashrc
+    echo -e "  > Configured SU_BIN_FOLDER!\n"
+else
+    echo -e "  > Already configured SU_BIN_FOLDER!\n"
+fi
+
 echo "  > Apply changes to the current shell with source ~/.bashrc"
