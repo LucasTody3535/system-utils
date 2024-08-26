9c45b6339a36c7ada27535950e5005ac7ee5bea6
Toodie <lucas.vtodt@gmail.com>
2024-08-26 19:45:24 -0300

    add ansi_colors.sh file that contains ansi color codes to be used in script messages
    Details:
    - Each code is stored in a readonly variable
    - The color codes are formatted in a way Bash understands
    
    Benefits:
    - Prevents the need to redefine every color again in each script
    
    Usage:
    - Expected to be sourced from another script

---
+++ utils/ansi_colors.sh
@@ -0,0 +1,9 @@
+# shellcheck disable=SC2148
+# shellcheck disable=SC2034
+
+readonly FMT_RED_FONT='\033[1;31m'
+readonly FMT_BLUE_FONT='\033[0;34m'
+readonly FMT_GREEN_FONT='\033[1;32m'
+readonly FMT_YELLOW_FONT='\033[1;33m'
+
+readonly FMT_RESET='\033[0m'
