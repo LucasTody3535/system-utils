04e94a27af9a99f19bdbc6f5f88b1e1e7557d6fa
Toodie <lucas.vtodt@gmail.com>
2024-08-27 07:54:53 -0300

    add utility function which creates a folder and put files into it
    Details:
    - It uses global variables defined in ansi_colors.sh and ansi_styles.sh
    - Logs messages related to the move operation

--- utils/files.sh
+++ utils/files.sh
@@ -24,3 +24,54 @@ function check_file_existence() {
     done
     return 1 # Means that a file has not been found
 }
+
+#######################################
+# Creates a directory and moves files to it
+#
+# Arguments:
+#   $1 - A folder name
+#   $2 - A string of file extensions separated by commas
+#
+# Globals:
+#   FMT_BLUE_FONT - Defined in ansi_colors.sh
+#   FMT_GREEN_FONT - Defined in ansi_colors.sh
+#   FMT_RED_FONT - Defined in ansi_colors.sh
+#   FMT_RESET - Defined in ansi_styles.sh
+#   BASH_VERSION
+#
+#######################################
+function move_files() {
+    local dirname="$1"
+    local extensions="$2"
+    local file_search_action_status=1
+    local files_move_action_status=1
+
+    read -ra file_extensions <<< "${extensions}"
+
+    check_file_existence "${file_extensions[@]}"
+    file_search_action_status="$?"
+
+    # Create directory only if there are files with the given extensions
+    # else return and do not do anything
+    if [[ $file_search_action_status -eq 0 ]]; then
+        # Check if the folder already exists
+        if [[ -d "${dirname}" ]]; then
+            printf " > Folder %b%s already created%b! Moving files...\n" "${FMT_BLUE_FONT}" "${dirname}" "${FMT_RESET}"
+        else
+            mkdir -p "${dirname}"
+            printf " > Folder %b%s created%b! Moving files...\n" "${FMT_GREEN_FONT}" "${dirname}" "${FMT_RESET}"
+        fi
+    else
+        printf " > Folder %b%s not created%b! There are no files to put in.\n" "${FMT_RED_FONT}" "${dirname}" "${FMT_RESET}"
+        return
+    fi
+
+    # Move files to the created folder
+    for extension in "${file_extensions[@]}"; do
+        mv ./*."${extension}" "${dirname}" 2> /dev/null # Redirecting stderr to null
+        files_move_action_status="$?"
+        if [[ $files_move_action_status -eq 0 ]]; then
+            printf " > %bFiles with the extension .%s moved!%b\n" "${FMT_GREEN_FONT}" "${extension}" "${FMT_RESET}"
+        fi
+    done
+}
