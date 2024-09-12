6579cb06c93564dd0587d095748924d518c5562a
Toodie <lucas.vtodt@gmail.com>
2024-09-12 15:32:24 -0300

    improve environment setup
    Details:
    - Removed configuration of SU_UTILS_FOLDER as it is no longer necessary.
    - Replaced direct checks with grep to find the scripts folder in PATH.
    
    Benefits:
    - Avoids unnecessary modifications to the user's environment.
    - Simplifies and streamlines the setup script.

--- setup.sh
+++ setup.sh
@@ -5,27 +5,12 @@ clear
 echo -e "\n Starting configuration..."
 echo -e " It will affect only the .bashrc file\n"
 
-if [[ -z "$SU_UTILS_FOLDER" ]]; then
+ # shellcheck disable=SC2155
+readonly SU_BIN_FOLDER="$(pwd)/bin"
 
-    # shellcheck disable=SC2155
-    readonly SU_UTILS_FOLDER=$(pwd)"/utils"
-
-    echo "export SU_UTILS_FOLDER=""$SU_UTILS_FOLDER" >> ~/.bashrc
-    echo -e "  > Configured SU_UTILS_FOLDER!\n"
-else
-    echo -e "  > Already configured SU_UTILS_FOLDER!\n"
-fi
-
-if [[ -z "$SU_BIN_FOLDER" ]]; then
-
-    # shellcheck disable=SC2155
-    readonly SU_BIN_FOLDER=$(pwd)"/bin"
-
-    echo "export SU_BIN_FOLDER=""$SU_BIN_FOLDER" >> ~/.bashrc
+if ! echo "$PATH" | grep -q "$SU_BIN_FOLDER"; then
     echo "export PATH=""$PATH":"$SU_BIN_FOLDER" >> ~/.bashrc
-    echo -e "  > Configured SU_BIN_FOLDER!\n"
+    echo " bash environment configuration complete!"
 else
-    echo -e "  > Already configured SU_BIN_FOLDER!\n"
+    echo " bash environment already configured!"
 fi
-
-echo "  > Apply changes to the current shell with source ~/.bashrc"
